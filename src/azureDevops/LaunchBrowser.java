d6d4c37b5c6caead6d236fef90eed069813272d9
sandy <sandeepmaurya233@gmail.com>
2024-09-03 15:02:18 +0530

    first

---
+++ src/azureDevops/LaunchBrowser.java
@@ -0,0 +1,24 @@
+package azureDevops;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+
+        // Initialize the Chrome WebDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Open a website
+        driver.get("https://www.google.com");
+
+        // Print the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+
+        // Close the browser
+        driver.quit();
+    }
+}
